785f277a5b47605a2a8d369be96f33cb54385a65
True Ghiassi <true@ghiassitrio.co.uk>
2022-07-11 22:49:36 +0100

    A test for a main function.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,24 @@ module Main (main) where
 
 import qualified Test.Tasty as Tasty
 import qualified Test.Tasty.HUnit as HUnit
+import qualified Data.Text as T
+import qualified Parser
+import qualified Text.Megaparsec as M
 
 main :: IO ()
 main =
     Tasty.defaultMain $
-    HUnit.testCase "greet" $ do
-        2 + 2 HUnit.@?= 4
+    HUnit.testCase "mainFunction" $ do
+        M.parse Parser.parser "" mainFunction HUnit.@?= Right ()
+
+
+mainFunction :: T.Text
+mainFunction =
+    "func main() {\n\
+    \\tGO <- Cmds([]Cmd{\n\
+    \\t\tfmtPrintln(\"What is your first name?\"),\n\
+    \\t\tfmtScanln{}}).RUN\n\
+    \\tfor {\n\
+    \\t\tgo (<-GO)()\n\
+    \\t}\n\
+    \}"
